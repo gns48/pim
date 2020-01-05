23e3dfe5de2f7f78db38577598e4762b5b5803d5
Gleb Semenov <gleb.semenov@gmail.com>
2020-01-05 22:34:28 +0300

    global definitions file added

---
+++ src/pim.h
@@ -0,0 +1,35 @@
+/**
+ * @file   pim.h 
+ * @author Gleb Semenov <gleb.semenov@gmail.com>
+ * @date   Sun Jan  5 22:24:09 2020
+ * 
+ * @brief  Process integrity checker, Global definitions
+ * 
+ * 
+ */
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
+#include <linux/kallsyms.h>
+#include <linux/mm.h>
+#include <linux/time.h>
+#include <linux/delay.h>
+#include <linux/random.h>
+#include <linux/uaccess.h>
+
+#include "timing.h"
+#include "hashtable.h"
+#include "siphash.h"
+
+
+#ifndef __PIM_H__
+#define __PIM_H__
+
+
+#define MODULO50 50  // At every 50th wake up we dump hash table
+#define 
+
+#endif // #ifndef __PIM_H__
